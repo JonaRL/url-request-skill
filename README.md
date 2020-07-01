c82d263d2e2e0ab78ecfb1ca79cab2085e803d9d
JonaRL <48405537+JonaRL@users.noreply.github.com>
2020-07-01 16:43:33 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,17 +1,2 @@
-# <img src="https://raw.githack.com/FortAwesome/Font-Awesome/master/svgs/solid/globe.svg" card_color="#8CE0FE" width="50" height="50" style="vertical-align:bottom"/> Url Request
-Makes url requests. requests can be defined in the skill settings.
-
-## About
-This skill is able to make url requests when a custom phrase is said.
-
-## Examples
-* "Make a request"
-
-## Credits
-JonaRL
-
-## Category
-**IoT**
-
-## Tags
-
+# <img src='https://raw.githack.com/FortAwesome/Font-Awesome/master/svgs/solid/globe.svg' card_color='#22a7f0' width='50' height='50' style='vertical-align:bottom'/> Under Development
+Lorem ipsum dolor...
--- __init__.py
+++ __init__.py
@@ -1,15 +1,35 @@
-from mycroft import MycroftSkill, intent_file_handler
+# Copyright 2017, Mycroft AI Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
+from os.path import dirname, join
 
-class UrlRequest(MycroftSkill):
+import requests
+
+from adapt.intent import IntentBuilder
+from mycroft.skills.core import MycroftSkill, intent_handler
+from random import choice
+
+class URLRequest(MycroftSkill):
     def __init__(self):
-        MycroftSkill.__init__(self)
+        super(URLRequest, self).__init__(name="URLRequest")
 
-    @intent_file_handler('request.url.intent')
-    def handle_request_url(self, message):
-        self.speak_dialog('request.url')
+    @intent_handler(IntentBuilder("RequestIntent").require("URL"))
+    def make_request(self, message):
+        self.speak(self.lang[:-3], "Test")
 
+    def stop(self):
+        pass
 
 def create_skill():
-    return UrlRequest()
-
+    return URLRequest()
